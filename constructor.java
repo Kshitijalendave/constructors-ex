5c7b52b0081951e00736c1d5ac447d265c55ee95
Kshitijalendave <144642390+Kshitijalendave@users.noreply.github.com>
2025-08-18 23:25:10 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class constructor {
+int x;
+public constructor() {
+x=8;
+}
+	public static void main(String[] args) {
+		constructor myObj=new constructor();
+		System.out.println(myObj.x);
+	}
+
+}
